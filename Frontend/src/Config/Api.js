40ea4ac2fdfdc8f2755bb6a0c3f4ba2e23941bda
zaakiraza <raaza.zaakir@gmail.com>
2026-02-16 10:47:01 +0500

    profile

--- Frontend/src/Config/Api.js
+++ Frontend/src/Config/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5004/api";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5004/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
